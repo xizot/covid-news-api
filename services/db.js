3d5f629a65656c647848b29d3cd76a4a02206c99
unknown <nvnhat.17ck1@gmail.com>
2021-06-06 15:33:56 +0700

    fix connect to postgre heroku

--- services/db.js
+++ services/db.js
@@ -1,5 +1,22 @@
 const Sequelize = require("sequelize");
 const connectionString = process.env.DATABASE_URL;
 
-const db = new Sequelize(connectionString);
+//LOCAL
+// const db = new Sequelize(connectionString);
+
+//HEROKU
+const db = new Sequelize({
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
 module.exports = db;
